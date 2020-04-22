5ae20e551fc2a9388d883e786a2d47a4e173dd4a
mohanbonam <mohan@users.noreply.github.com>
2020-04-22 14:54:14 -0500

    app theme

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,43 @@ export default {
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 
 #app {
-  margin: 100px 0 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   font-family: monospace;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   text-align: center;
+  background-color: rgb(49, 49, 49);
+  /* margin: 100px 0 0; */
+  /* font-family: monospace;
+  text-align: center;
+  display: flex;
+  background-color: brown;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center; */
   /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px; */
+
+  /* display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column; */
 }
 </style>
